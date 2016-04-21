bf5d9c449bf834b676af60ffc4d138324b2abd34
muj <muj@mujui-MacBook-Pro.local>
2016-04-21 14:22:11 +0900

    1. DBConfig 파일 DB name, id, pw수정  2. server.java getProperty 수정

--- src/test/Server/Server.java
+++ src/test/Server/Server.java
@@ -7,34 +7,37 @@ import utill.XmlUtill;
 
 public class Server {
 	public static void main(String[] args) {
-		String config = System.getProperty("DBconfig");
+		String config = System.getProperty("user.dir")+"/config/DBConfig.xml";
     	String rootName = "MYSQLConfig";
     	
+    	
+    	System.out.println("config: "+config);
     	if(config != null && !config.equals("")){
     		
-    	MYSQLConnector connector = MYSQLConnector.getInstance();
-    	XmlUtill xml = XmlUtill.getInstance();
-    	
-    	xml.init(config, rootName);
-    	
-    	String serverip = xml.getAttributeValue("ip");
-    	String port = xml.getAttributeValue("port");
-    	String dbName = xml.getAttributeValue("databaseName");
-    	String id = xml.getAttributeValue("id");
-    	String password = xml.getAttributeValue("password");
-    	
-    	if(!connector.isConnected()){
-			try {
-				connector.connection(serverip, port, dbName, id, password);
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			} catch (SQLException e) {
-				e.printStackTrace();
+	    	MYSQLConnector connector = MYSQLConnector.getInstance();
+	    	XmlUtill xml = XmlUtill.getInstance();
+	    	
+	    	xml.init(config, rootName);
+	    	
+	    	String serverip = xml.getAttributeValue("ip");
+	    	String port = xml.getAttributeValue("port");
+	    	String dbName = xml.getAttributeValue("databaseName");
+	    	String id = xml.getAttributeValue("id");
+	    	String password = xml.getAttributeValue("password");
+	    	
+	    	if(!connector.isConnected()){
+				try {
+					connector.connection(serverip, port, dbName, id, password);
+					System.out.println("db connect!");
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 			}
-		}
     	
     	}
 		
-		new AcceptThread(10000, "AcceptThread").start();
+		new AcceptThread(5000, "AcceptThread").start();
 	}
 }
